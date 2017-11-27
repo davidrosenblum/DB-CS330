3f810950e95e261dd94dec66cc7067e57a9c825d
davidrosenblum <david@rosenblum.net>
2017-11-27 01:30:54 -0500

    DB-27 'massive' front-end redesign

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -1,4 +1,15 @@
 var client = (function(){
+    Set.prototype.intersection = function(set){
+        var intersection = new Set();
+
+        for(var k of set){
+            if(this.has(k)){
+                intersection.add(k);
+            }
+        }
+        return intersection;
+    };
+
     var resultsContainer, modal, modalBody, modalDarkness;
 
     // bakset object ("my list")
@@ -18,6 +29,8 @@ var client = (function(){
                 element.appendChild(itemFigure.figure);
 
                 saveLocalBasket();
+
+                commonAssociationManager.store(name);
             }
         },
         remove: function(name){
@@ -37,6 +50,8 @@ var client = (function(){
                 }
 
                 saveLocalBasket();
+
+                commonAssociationManager.remove(name);
             }
         },
         getElement: function(){
@@ -44,6 +59,85 @@ var client = (function(){
         }
     };
 
+    var commonAssociationManager = {
+        sets: {},
+        associations: null,
+        store: function(name){
+            if(typeof this.sets[name] !== "undefined") return;
+
+            var that = this;
+            CCAPI.requestCuisineAssociations(name, function(res, status){
+                if(status !== 400){
+                    try{
+                        that.sets[name] = JSON.parse(res); // its an array!
+                    }
+                    catch(err){
+                        return;
+                    }
+
+                    // will auto populate the associations array if its null
+                    that.sort();
+                    that.updateDOM();
+                }
+            });
+        },
+        remove: function(name){
+            if(name in this.sets){
+                delete this.sets[name];
+
+                // reset associations - will be re-populated in sort
+                this.associations = null;
+
+                this.sort();
+                this.updateDOM();
+            }
+        },
+        sort: function(){
+            for(var name in this.sets){
+                var set = this.sets[name]; // set is an array
+                this.extractIntersection(set);
+            }
+        },
+        extractIntersection(array){
+            // special case for when the associations array is null
+            if(!this.associations){
+                this.associations = array;
+                return;
+            }
+
+            // find what elements match, and make that the new associations
+            var intersection = [];
+            for(var i = 0; i < this.associations.length; i++){
+                for(var j = 0; j < array.length; j++){
+                    if(this.associations[i] === array[j]){
+                        intersection.push(array[j]);
+                    }
+                }
+            }
+            this.associations = intersection;
+        },
+        updateDOM: function(){
+            // extract the container
+            var element = document.querySelector("#common-associations-container");
+            if(!element) throw new Error("Missing element!");
+
+            // wipe the html elements
+            element.innerHTML = "";
+
+            // for each association... create a display figure
+            for(var i = 0; i < this.associations.length; i++){
+                var df = new DisplayFigure(this.associations[i]);
+                element.appendChild(df.figure);
+            }
+
+            // no association
+            if(element.innerHTML.length === 0){
+                //element.innerHTML = "(None)"; depricated?
+            }
+        }
+    };
+
+    // display figure class - a container for the cuisine name & buttons
     var DisplayFigure = function(name){
         this.figure = createItemBlock(name);
         this.addBtn = createAddButton();
@@ -52,6 +146,7 @@ var client = (function(){
         this.name = name;
 
         var that = this; // preserve scope
+
         this.addBtn.addEventListener("click", function(evt){
             that.showRemove();
             basket.add(name);
@@ -79,8 +174,10 @@ var client = (function(){
         this.figure.parentNode.removeChild(this.figure);
     };
 
+    // dictionary of all display figures, prevents querying for them later
     var displayFigures = {};
 
+    // submits the search to the server (triggered by enter on search or search button click)
     var search = function(){
         // extract search data from DOM
         var searchInput = document.querySelector("#search-input").value,
@@ -126,6 +223,7 @@ var client = (function(){
 
     // creates the HTMLUListElement that contains all the list items
     var createItemList = function(data){
+        // extract the data (data is a json string)
         try{
             data = JSON.parse(data);
         }
@@ -135,9 +233,11 @@ var client = (function(){
 
         displayFigures = {};
 
+        // create the html unordered list element
         var list = document.createElement("ul");
         list.classList.add("results-list");
 
+        // populate the list with list items containing the display figures
         for(var i = 0; i < data.length; i++){
             var li = document.createElement("li"),
                 figureObject = new DisplayFigure(data[i]);
@@ -153,6 +253,7 @@ var client = (function(){
 
     // creates the HTMLTableElement for a cuisine item
     var createInfoTable = function(data){
+        // parse the data (data is a json string)
         try{
             data = JSON.parse(data);
         }
@@ -160,12 +261,14 @@ var client = (function(){
             return null;
         }
 
+        // create the table
         var tbl = document.createElement("table");
         tbl.setAttribute("class", "table");
 
         var tbody = document.createElement("tbody");
         tbl.appendChild(tbody);
 
+        // create a row for each attribute
         for(var attr in data){
             if(data[attr] !== null && attr !== "id"){
                 var tr = document.createElement("tr");
@@ -206,7 +309,7 @@ var client = (function(){
     // creates an HTMLButtonElement for cuisine item details
     var createInfoButton = function(name){
         var infoBtn = document.createElement("button");
-        infoBtn.classList.add("btn-info");
+        infoBtn.classList.add("cc-btn-info");
         infoBtn.innerHTML = "Details";
         infoBtn.addEventListener("click", function(evt){
             // button clicked...
@@ -229,8 +332,7 @@ var client = (function(){
     var createAddButton = function(name){
         var addBtn = document.createElement("button");
         addBtn.innerHTML = "Add";
-        addBtn.classList.add("btn-warning");
-        addBtn.classList.add("add-btn");
+        addBtn.classList.add("cc-btn-add");
         return addBtn;
     };
 
@@ -238,8 +340,7 @@ var client = (function(){
     var createRemoveButton = function(name){
         var removeBtn = document.createElement("button");
         removeBtn.innerHTML = "Remove";
-        removeBtn.classList.add("btn-danger");
-        removeBtn.classList.add("remove-btn");
+        removeBtn.classList.add("cc-btn-remove");
         return removeBtn;
     };
 
@@ -278,7 +379,8 @@ var client = (function(){
         }
     };
 
-    var displayModal = function(html){
+    // shows the modal and sets its initial information display
+    var displayModal = function(html, title){
         resultsContainer.style.display = "none";
         modal.style.visibility = "visible";
         modalDarkness.style.visibility = "visible";
@@ -290,8 +392,13 @@ var client = (function(){
             modalBody.innerHTML = "";
             modalBody.appendChild(html);
         }
+
+        if(typeof title === "string"){
+            modalHeader.innerHTML = "<h2>" + title + "</h2>";
+        }
     };
 
+    // appends new information to the already opened modal
     var appendModal = function(html){
         if(typeof html === "string"){
             modalBody.innerHTML += html;
@@ -301,12 +408,14 @@ var client = (function(){
         }
     };
 
+    // closes (hides) the popup modal and clears its contents
     var closeModal = function(){
         modalBody.innerHTML = "";
         modal.style.visibility = "hidden";
         modalDarkness.style.visibility = "hidden";
     };
 
+    // saves current basket items to local storage
     var saveLocalBasket = function(){
         var data = [];
         for(var name in basket.cuisines){
@@ -316,12 +425,46 @@ var client = (function(){
         window.localStorage.setItem("cuisine-crusader", JSON.stringify(data));
     };
 
+    // loads basket items from local storage
     var loadLocalBasket = function(){
         var save = window.localStorage.getItem("cuisine-crusader");
 
         var data = JSON.parse(save);
         for(let i = 0; i < data.length; i++){
             basket.add(data[i]);
+        }
+    };
+
+    // wipes the local storage for this app
+    var deleteLocalBasket = function(){
+        window.localStorage.removeItem("cuisine-crusader");
+    };
+
+    // shows the user profile
+    var showProfile = function(){
+        displayModal(
+            "<p>This feature is not yet supported.</p.",
+            "Profile"
+        )
+    };
+
+    // shows the instructions modal
+    var showHelp = function(){
+        displayModal(
+            "<p><b>Cuisine</b> searches will find a cuisine by names matching the search.</p>" +
+            "<p><b>Taste</b> searches will find cuisines that are associationed with the taste.</p>" +
+            "<p><b>Technique</b> searches will find a cuisine that are associated with the technique.</p>",
+            "Instructions"
+        );
+    };
+
+    // injects the minigame
+    var createMinigame = function(){
+        // inject the minigame script if it does not already exist
+        if(!document.querySelector("script[src='js/minigame.js']")){
+            var script = document.createElement("script");
+            script.setAttribute("src", "js/minigame.js");
+            document.body.appendChild(script);
         }
     };
 
@@ -342,16 +485,32 @@ var client = (function(){
     };
 
     var init = function(){
+        // search button should search when clicked...
         document.querySelector("#search-btn").addEventListener("click", search);
 
+        // enter key in search input also triggers search function
+        document.querySelector("#search-input").addEventListener("keyup", function(evt){
+            if(evt.keyCode === 13){ // 13 = enter key
+                search();
+            }
+        });
+
+        // query and store html elements that are commonly used...
         resultsContainer = document.querySelector("#results-container");
         modal = document.querySelector(".cc-modal");
         modalBody = document.querySelector(".cc-modal-body");
+        modalHeader = document.querySelector(".cc-modal-header");
         modalDarkness = document.querySelector(".cc-modal-dark-bg");
 
+        // modal close buttons should close modal when clicked...
         document.querySelector("#modal-btn").addEventListener("click", closeModal);
         document.querySelector("#close-modal").addEventListener("click", closeModal);
 
+        // nav buttons
+        document.querySelector("#profile-btn").addEventListener("click", showProfile);
+        document.querySelector("#help-btn").addEventListener("click", showHelp);
+
+        // load last basket
         loadLocalBasket();
 
         // parse the query strings...
@@ -361,6 +520,9 @@ var client = (function(){
             document.querySelector("#search-input").value = qs["search"];
             document.querySelector("#search-btn").click();
         }
+
+        // minigame easter egg!
+        //document.querySelector("footer").onclick = createMinigame;
     };
     window.addEventListener("load", init);
 
@@ -373,7 +535,9 @@ var client = (function(){
         " \\_____________________________________/"
     );
 
+    // return public methods
     return {
-        basket: () => basket
+        basket: () => basket,
+        associations: () => commonAssociationManager.associations
     };
 })();
